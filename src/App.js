0a637524d473877caf26861bc090b786b8970c6e
constyejin <lightup.jin@gmail.com>
2024-01-19 01:14:31 +0900

    📝 구현해야 될 로직 정리

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Main from './component/Main';
 import Score from './component/Score';
 import Buttons from './component/Buttons';
 
+// Score (user, computer)
+// 1. User와 Computer가 선택한 값을 비교해서 승부를 가린다. (win, lose, tie)
+// 2. win하면 score + 1 시킨다.
+// 3. 로컬스토리지에 값을 저장하고 Reset 버튼을 눌렀을 때 값을 초기화 시킨다.
+
+// Main (User선택값, Computer 선택값)
+// 1. Buttons값 중에 유저가 클릭한 값을 화면에 보여준다.
+// 2. Computer 값은 세 값 중 하나가 랜덤으로 선택되게 한다.
+// 3. 1,2번 값을 비교해서 승부를 가린다 (win, lose, tie)
+// 4. 결과에 따라 win, lose, tie 글씨 컬러를 변경한다. (class 적용)
 const choice = {
   rock : {
     name : "Rock",
