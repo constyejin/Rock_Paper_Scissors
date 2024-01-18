c9820501b84099df9e3de47f54a449a1fa801090
constyejin <lightup.jin@gmail.com>
2024-01-18 23:46:40 +0900

    ✨ Create Buttons component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from './component/Main';
 import Score from './component/Score';
+import Buttons from './component/Buttons';
 
 function App() {
   return (
     <div>
       <Score />
       <Main />
+      <Buttons />
     </div>
   );
 }
---
+++ src/component/Buttons.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Buttons = () => {
+  return (
+    <div className='btn-list'>
+      <button>가위
+        <img src="" alt="" />
+      </button>
+      <button>보
+        <img src="" alt="" />
+      </button>
+      <button>바위
+        <img src="" alt="" />
+      </button>
+    </div>
+  )
+}
+
+export default Buttons
