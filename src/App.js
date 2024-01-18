5e949fab2b1808e7199bfe2fd9bfafcd672e5136
constyejin <lightup.jin@gmail.com>
2024-01-18 23:44:43 +0900

    ✨ Create Score component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './component/Main';
+import Score from './component/Score';
 
 function App() {
   return (
     <div>
-
+      <Score />
+      <Main />
     </div>
   );
 }
---
+++ src/component/Score.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Score = () => {
+  return (
+    <div className='score-box'>
+      <div className="score">
+        <h3>Score</h3>
+        <h3>0</h3>
+        <h3>:</h3>
+        <h3>0</h3>
+      </div>
+      <button>Reset</button>
+    </div>
+  )
+}
+
+export default Score
