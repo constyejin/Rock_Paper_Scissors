7db645c248b696823d35a817cd8e587fd65b6631
constyejin <lightup.jin@gmail.com>
2024-01-27 16:27:10 +0900

    📝 common -> main.scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './assets/scss/_common.scss';
+import './assets/scss/main.scss';
 import Main from './component/Main';
 import Score from './component/Score';
 import Buttons from './component/Buttons';
