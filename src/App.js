7826d99979d12061048e585178643834e04ea1bb
constyejin <lightup.jin@gmail.com>
2024-02-06 02:08:46 +0900

    📝 Edit css path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './assets/scss/main.scss';
+import './App.css';
 import Main from './component/Main';
 import Score from './component/Score';
 import Buttons from './component/Buttons';
